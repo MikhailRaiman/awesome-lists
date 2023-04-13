4f3db09a74ce09d8431ea7b2dbe5b41e13812087
MikhailRaiman <mikhail.raiman@gmail.com>
2023-04-13 18:19:19 +0300

    drug and drop

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,13 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 export class HomeComponent implements OnInit {
   data: Topic[] = [
     {id: "1", name: 'Card1'},
-    {id: "2", name: 'Card2'}
+    {id: "2", name: 'Card2'},
+    {id: "3", name: 'Card3'},
+    {id: "4", name: 'Card4'},
+    {id: "5", name: 'Card5'},
+    {id: "6", name: 'Card6'},
+    {id: "7", name: 'Card7'},
+    {id: "8", name: 'Card8'}
   ];
   closeResult = '';
   constructor(private offcanvasService: NgbOffcanvas) { }
